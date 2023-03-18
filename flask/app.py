3c57c279cb42e0ce220f19dc199d1b927272d4ac
Anh-Dnt <dntanh2505@gmail.com>
2023-03-18 23:14:40 +0700

    hidden PostgreSQL URI

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,5 @@
 import flask_cors
+import os
 
 from flask import Flask, request, jsonify
 from ariadne import graphql_sync
@@ -8,7 +9,7 @@ from gql.places import schema, db
 app = Flask(__name__)
 flask_cors.CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://wznqynqx:0hWe0Nfx3r8lPlhuMzCRMa6ZyXH0j-5_@floppy.db.elephantsql.com/wznqynqx"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DB_POSTGRE_URL', default=None)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
@@ -25,7 +26,7 @@ def flask_health_check():
 	return "success"
 
 # Create a GraphQL endpoint for executing GraphQL queries
-@app.route("/graphql", methods=["POST"])
+@app.route("/countries", methods=["POST"])
 def graphql_server():
    data = request.get_json()
    success, result = graphql_sync(schema, data, context_value={"request": request})
